50bdfbb251fa57cab2a68c1fbeab89001504e899
Gaspar Tartari <gaspartartari@gmail.com>
2024-02-02 21:14:30 -0300

    Entities

---
+++ backend/src/main/java/com/myprojects/financecontrol/entities/Expense.java
@@ -0,0 +1,69 @@
+package com.myprojects.financecontrol.entities;
+
+import java.time.Instant;
+
+public class Expense {
+    
+    private Long id;
+    private Double ammount;
+    private Instant date;
+
+    public Expense(){
+
+    }
+
+    public Expense(Long id, Double ammount, Instant date) {
+        this.id = id;
+        this.ammount = ammount;
+        this.date = date;
+    }
+
+    public Double getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(Double ammount) {
+        this.ammount = ammount;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Expense other = (Expense) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ backend/src/main/java/com/myprojects/financecontrol/entities/User.java
@@ -0,0 +1,97 @@
+package com.myprojects.financecontrol.entities;
+
+import java.time.Instant;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public User(){
+
+    }
+
+    public User(Long id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
